74eae24e4517c34cde90bcff9723cec9c571dcc1
Henry01270 <118987076+Henry01270@users.noreply.github.com>
2025-02-19 10:17:34 +0000

    25_0219_1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,6 +4,7 @@ import {Routes, Route, Link} from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Counter from './pages/Counter';
+import Input from "./pages/Input";
 
 function App() {
     return (
@@ -11,12 +12,14 @@ function App() {
             <nav>
                 <Link to="/">Home</Link> |
                 <Link to="/about"> about</Link> |
-                <Link to="/counter"> counter</Link>
+                <Link to="/counter"> counter</Link> |
+                <Link to="input">input</Link>
             </nav>
             <Routes>
                 <Route path="/" element={<Home />}/>
                 <Route path="/about" element={<About />}/>
                 <Route path="/counter" element={<Counter />}/>
+                <Route path="/input" element={<Input />}/>
             </Routes>
         </div>
     );
